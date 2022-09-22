45b7160a5e6f60df8d8df1af53a15e4456f047b6
Leidanmty <ld.andrade@hotmail>
2022-09-22 00:00:05 -0500

    other changes in this proyected

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
 //Routers
+const { usersRoutes } = require("./routes/users.routes");
+const { restaurantsRoutes } = require("./routes/restaurants.routes");
+const { mealsRoutes } = require("./routes/meals.routes");
+const { ordersRoutes } = require("./routes/orders.routes");
 
 //Init our Express app
 const app = express();
@@ -9,6 +13,10 @@ const app = express();
 app.use(express.json());
 
 //Define Endpoints
+app.use("/api/v1/users", usersRoutes);
+app.use("/api/v1/restaurants", restaurantsRoutes);
+app.use("/api/v1/meals", mealsRoutes);
+app.use("/api/v1/orders", ordersRoutes);
 
 //Catch non-existing endpoints
 app.all("*", (req, res) => {
